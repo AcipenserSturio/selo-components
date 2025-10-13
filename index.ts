b8c7c80daf75e0bd1dd6a1a8d39d8a610edeea27
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-10-13 23:11:49 +0300

    First components

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-import MyComponent from './src/MyComponent.astro';
-
-export default MyComponent;
+export { default as BaseHead } from "@/components/BaseHead.astro";
+export { default as DarkModeToggle } from "@/components/DarkModeToggle.astro";
+export { default as Embed } from "@/components/Embed.astro";
+export { default as Footer } from "@/components/Footer.astro";
+export { default as Header } from "@/components/Header.astro";
