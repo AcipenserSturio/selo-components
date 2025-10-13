fce2e51d9ca24ee96749c57db94fd352aec4b469
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-10-13 23:26:12 +0300

    don't use aliases i guess?

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-export { default as BaseHead } from "@/components/BaseHead.astro";
-export { default as DarkModeToggle } from "@/components/DarkModeToggle.astro";
-export { default as Embed } from "@/components/Embed.astro";
-export { default as Footer } from "@/components/Footer.astro";
-export { default as Header } from "@/components/Header.astro";
+export { default as BaseHead } from "./src/components/BaseHead.astro";
+export { default as DarkModeToggle } from "./src/components/DarkModeToggle.astro";
+export { default as Embed } from "./src/components/Embed.astro";
+export { default as Footer } from "./src/components/Footer.astro";
+export { default as Header } from "./src/components/Header.astro";
