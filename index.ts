7839690b1091c39b5bf366530dbb58d8f3c676f1
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-10-14 01:22:43 +0300

    import style

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
+import "./src/styles/common.css";
+
 export { default as BaseHead } from "./src/components/BaseHead.astro";
 export { default as DarkModeToggle } from "./src/components/DarkModeToggle.astro";
 export { default as Embed } from "./src/components/Embed.astro";
