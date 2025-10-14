6224795fae8e64fa6c5c2262ac25f8c6fba1f040
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-10-14 12:29:51 +0300

    add style path specifier

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-import "./src/styles/common.css";
+// import "./src/styles/common.css";
 
 export { default as BaseHead } from "./src/components/BaseHead.astro";
 export { default as DarkModeToggle } from "./src/components/DarkModeToggle.astro";
 export { default as Embed } from "./src/components/Embed.astro";
 export { default as Footer } from "./src/components/Footer.astro";
 export { default as Header } from "./src/components/Header.astro";
+
